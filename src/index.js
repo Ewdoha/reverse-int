2b136ed29e172afab37e6734cfc84b3220b24f93
Ewdoha <olexiyewdoshchenko@gmail.com>
2022-01-10 22:46:32 +0200

    init:start reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let toStr = n.toString();
+  let toSplit = toStr.split('');  
+  let toRevers = toSplit.reverse();
+  let result = 0; 
+  for (let i = 0; i < toRevers.length; i++) {
+   result += toRevers[i];
+  }
+   return parseInt(result.slice(1,10));
 }
